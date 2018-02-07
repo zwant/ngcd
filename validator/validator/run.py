353b6878fe4af282154cc1bd783430297c6900eb
Svante Paldan <svante@paldan.se>
2018-02-07 17:18:24 +0100

    Adding python logger for the validator app

--- validator/validator/run.py
+++ validator/validator/run.py
@@ -4,6 +4,9 @@ import sys
 import os
 from collections import namedtuple
 from ngcd_common import events_pb2
+import logging
+
+logger = logging.getLogger('validator')
 
 QueueConfig = namedtuple('QueueConfig', ['exchange', 'name', 'routing_key', 'message_clz'])
 
@@ -36,7 +39,9 @@ ALL_QUEUE_CONFIGS = [
      QueueConfig('internal', 'internal.artifact.publish', 'artifact.publish', events_pb2.ArtifactPublished)),
 
 ]
+
 def main():
+    setup_logging()
     rabbitmq_host = os.environ['RABBITMQ_HOST']
     connection_established = False
     while not connection_established:
@@ -44,7 +49,7 @@ def main():
             connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
             connection_established = True
         except pika.exceptions.ConnectionClosed:
-            print('Unable to connect to RabbitMQ host {}. Will retry in a second!'.format(rabbitmq_host))
+            logger.error('Unable to connect to RabbitMQ host {}. Will retry in a second!'.format(rabbitmq_host))
             import time
             time.sleep(1)
 
@@ -57,12 +62,26 @@ def main():
                              durable=True,
                              exchange_type='topic')
 
-    print('Setting up {} converters'.format(len(ALL_QUEUE_CONFIGS)))
+    logger.info('Setting up {} converters'.format(len(ALL_QUEUE_CONFIGS)))
     for src_config, dst_config in ALL_QUEUE_CONFIGS:
         setup_converter(channel, src_config, dst_config)
-    print(' [*] Waiting for data. To exit press CTRL+C')
+    logger.info('[*] Waiting for data. To exit press CTRL+C')
 
     channel.start_consuming()
+
+def setup_logging():
+    import logging.config
+    import yaml
+    path = 'validator/logging.yaml'
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            try:
+                config = yaml.safe_load(f.read())
+            except Exception as e:
+                raise Exception('Failed to read log config from {}'.format(path), e)
+    else:
+        raise Exception('Failed to read log config from {}'.format(path))
+    logging.config.dictConfig(config)
 
 def get_headers(queue_config):
     return {'proto-message-type': str(queue_config.message_clz.__name__)}
@@ -78,7 +97,7 @@ def create_callback(src_queue_config, dst_queue_config):
                             headers=headers
                          ))
         ch.basic_ack(delivery_tag = method.delivery_tag)
-        print('Forwarded 1 message from {} to {}'.format(src_queue_config.name, dst_queue_config.name))
+        logger.debug('Forwarded 1 message from {} to {}'.format(src_queue_config.name, dst_queue_config.name))
     return callback
 
 def create_queue_from_config(channel, queue_config):
